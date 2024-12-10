c0fc169136d7fa720b9ec83922f82725c4a880c5
Tobias Herlitz <tobiasherlitz@gmail.com>
2024-12-10 12:39:30 +0100

    using empty structs instead of bool in uniqueness map

--- 2024/08/main.go
+++ 2024/08/main.go
@@ -9,9 +9,9 @@ func withinBounds(grid [][]rune, y int, x int) bool {
 	return x >= 0 && x < len(grid[0]) && y >= 0 && y < len(grid)
 }
 
-func antinodesFromAntenna(grid [][]rune, y int, x int) map[string]bool {
+func antinodesFromAntenna(grid [][]rune, y int, x int) map[string]struct{} {
 	antenna := grid[y][x]
-	antinodes := make(map[string]bool)
+	antinodes := make(map[string]struct{})
 	for opposingY, column := range grid {
 		for opposingX, cell := range column {
 			if antenna != cell || (y == opposingY && x == opposingX) {
@@ -22,7 +22,7 @@ func antinodesFromAntenna(grid [][]rune, y int, x int) map[string]bool {
 			antinodeX := x - (opposingX - x)
 
 			if withinBounds(grid, antinodeY, antinodeX) {
-				antinodes[fmt.Sprintf("%v:%v", antinodeX, antinodeY)] = true
+				antinodes[fmt.Sprintf("%v:%v", antinodeX, antinodeY)] = struct{}{}
 			}
 		}
 	}
@@ -31,13 +31,13 @@ func antinodesFromAntenna(grid [][]rune, y int, x int) map[string]bool {
 }
 
 func countAntinodes(grid [][]rune) int {
-	antinodes := make(map[string]bool)
+	antinodes := make(map[string]struct{})
 	for y, column := range grid {
 		for x, cell := range column {
 			if cell != '.' {
 				newAntinodes := antinodesFromAntenna(grid, y, x)
 				for key := range newAntinodes {
-					antinodes[key] = true
+					antinodes[key] = struct{}{}
 				}
 			}
 		}

e3191db1894b4825edf67f41e1211d447a0e5607
Tobias Herlitz <tobiasherlitz@gmail.com>
2024-12-15 12:22:23 +0100

    helper to load a grid of ints

--- shared/go/adventhelper/adventhelper.go
+++ shared/go/adventhelper/adventhelper.go
@@ -1,40 +1,64 @@
 package adventhelper
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func PrintStuff() {
-	fmt.Println("Stuff")
+type Grid [][]int
+type Coordinate struct {
+	X int
+	Y int
 }
 
-// func loadEquations() ([]equation, error) {
-// 	input, err := os.ReadFile("input")
-// 	if err != nil {
-// 		return nil, err
-// 	}
+// See if this can be generic. i.e. also return [][]rune
+func LoadGrid(filePath string) (Grid, error) {
+	input, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
 
-// 	var equations []equation
-// 	for _, equationRow := range strings.Split(string(input), "\n") {
-// 		equationParts := strings.Split(equationRow, ": ")
+	grid := [][]int{}
+	for _, rowInput := range strings.Split(string(input), "\n") {
+		row := []int{}
+		for _, cellInput := range rowInput {
+			cell, err := strconv.Atoi(string(cellInput))
+			if err != nil {
+				return nil, err
+			}
 
-// 		answer, err := strconv.ParseInt(equationParts[0], 10, 64)
-// 		if err != nil {
-// 			return nil, err
-// 		}
+			row = append(row, cell)
+		}
 
-// 		var operands []int64
-// 		for _, operandInput := range strings.Split(equationParts[1], " ") {
-// 			operand, err := strconv.Atoi(operandInput)
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			operands = append(operands, int64(operand))
-// 		}
+		grid = append(grid, row)
+	}
 
-// 		equations = append(equations, equation{
-// 			answer,
-// 			operands,
-// 		})
-// 	}
+	return grid, nil
+}
 
-// 	return equations, nil
-// }
+func (g Grid) ToString() string {
+	rowRepresentations := []string{}
+	for _, row := range g {
+		rowRepresentation := ""
+		for _, cell := range row {
+			rowRepresentation = fmt.Sprintf("%v%v", rowRepresentation, cell)
+		}
+		rowRepresentations = append(rowRepresentations, rowRepresentation)
+	}
+
+	return strings.Join(rowRepresentations, "\n")
+}
+
+func (g Grid) IsWithinBounds(coord Coordinate) bool {
+	return coord.Y >= 0 && coord.Y < len(g) && coord.X >= 0 && coord.X < len(g[coord.Y])
+}
+
+func (g Grid) ReadCell(coord Coordinate) (int, error) {
+	if !g.IsWithinBounds(coord) {
+		return 0, fmt.Errorf("coordinate [%d:%d] is out of bounds", coord.X, coord.Y)
+	}
+
+	return g[coord.Y][coord.X], nil
+}

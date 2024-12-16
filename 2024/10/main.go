65eba930b82721506eba58dc606865ed83a2ea8c
Tobias Herlitz <tobiasherlitz@gmail.com>
2024-12-16 09:42:15 +0100

    day 10 part 2

--- 2024/10/main.go
+++ 2024/10/main.go
@@ -32,23 +32,32 @@ func findStartingPoints(grid helper.Grid) []helper.Coordinate {
 	return startingPoints
 }
 
-func walkTrails(grid helper.Grid, position helper.Coordinate, uniqueEndpoints map[helper.Coordinate]struct{}) {
+func walkTrails(grid helper.Grid, position helper.Coordinate, endpoints *[]helper.Coordinate) {
 	cell, err := grid.ReadCell(position)
 	if err != nil {
 		return
 	}
 
 	if cell == 9 {
-		uniqueEndpoints[position] = Empty
+		*endpoints = append(*endpoints, position)
 	}
 
 	for _, direction := range directions {
 		neighbor := helper.Coordinate{X: position.X + direction.X, Y: position.Y + direction.Y}
 		neighborCell, err := grid.ReadCell(neighbor)
 		if err == nil && neighborCell == cell+1 {
-			walkTrails(grid, neighbor, uniqueEndpoints)
+			walkTrails(grid, neighbor, endpoints)
 		}
 	}
+}
+
+func toUnique(endpoints []helper.Coordinate) map[helper.Coordinate]struct{} {
+	uniqueEndpoints := make(map[helper.Coordinate]struct{})
+	for _, endpoint := range endpoints {
+		uniqueEndpoints[endpoint] = Empty
+	}
+
+	return uniqueEndpoints
 }
 
 func main() {
@@ -59,12 +68,15 @@ func main() {
 	}
 
 	startingPoints := findStartingPoints(grid)
-	totalEndpointsReached := 0
+	totalEndpoints := 0
+	totalTrails := 0
 	for _, startingPoint := range startingPoints {
-		uniqueEndpoints := make(map[helper.Coordinate]struct{})
-		walkTrails(grid, startingPoint, uniqueEndpoints)
-		totalEndpointsReached += len(uniqueEndpoints)
+		endpoints := []helper.Coordinate{}
+		walkTrails(grid, startingPoint, &endpoints)
+		totalTrails += len(endpoints)
+		totalEndpoints += len(toUnique(endpoints))
 	}
 
-	fmt.Printf("Part 1 - Total score of all trailheads is: %v\n", totalEndpointsReached)
+	fmt.Printf("Part 1 - Total score of all trailheads is: %v\n", totalEndpoints)
+	fmt.Printf("Part 2 - Total score of all trailheads is: %v\n", totalTrails)
 }
--- 2024/10/main_test.go
+++ 2024/10/main_test.go
@@ -26,7 +26,7 @@ func TestFindStartingPoints(t *testing.T) {
 	}
 }
 
-func TestWalkTrails(t *testing.T) {
+func TestPartOne(t *testing.T) {
 	exampleGrid := helper.Grid{
 		[]int{8, 9, 0, 1, 0, 1, 2, 3},
 		[]int{7, 8, 1, 2, 1, 8, 7, 4},
@@ -81,9 +81,73 @@ func TestWalkTrails(t *testing.T) {
 	}
 
 	for _, testCase := range tests {
-		uniqueEndpoints := make(map[helper.Coordinate]struct{})
-		walkTrails(exampleGrid, testCase.startingPoint, uniqueEndpoints)
-		got := len(uniqueEndpoints)
+		endpoints := []helper.Coordinate{}
+		walkTrails(exampleGrid, testCase.startingPoint, &endpoints)
+		got := len(toUnique(endpoints))
+		if got != testCase.expect {
+			t.Errorf("Incorrect number of trails found. got: %v, expect %v", got, testCase.expect)
+		}
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	exampleGrid := helper.Grid{
+		[]int{8, 9, 0, 1, 0, 1, 2, 3},
+		[]int{7, 8, 1, 2, 1, 8, 7, 4},
+		[]int{8, 7, 4, 3, 0, 9, 6, 5},
+		[]int{9, 6, 5, 4, 9, 8, 7, 4},
+		[]int{4, 5, 6, 7, 8, 9, 0, 3},
+		[]int{3, 2, 0, 1, 9, 0, 1, 2},
+		[]int{0, 1, 3, 2, 9, 8, 0, 1},
+		[]int{1, 0, 4, 5, 6, 7, 3, 2},
+	}
+
+	tests := []struct {
+		startingPoint helper.Coordinate
+		expect        int
+	}{
+		{
+			startingPoint: helper.Coordinate{X: 2, Y: 0},
+			expect:        20,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 4, Y: 0},
+			expect:        24,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 4, Y: 2},
+			expect:        10,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 6, Y: 4},
+			expect:        4,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 2, Y: 5},
+			expect:        1,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 5, Y: 5},
+			expect:        4,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 0, Y: 6},
+			expect:        5,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 6, Y: 6},
+			expect:        8,
+		},
+		{
+			startingPoint: helper.Coordinate{X: 1, Y: 7},
+			expect:        5,
+		},
+	}
+
+	for _, testCase := range tests {
+		endpoints := []helper.Coordinate{}
+		walkTrails(exampleGrid, testCase.startingPoint, &endpoints)
+		got := len(endpoints)
 		if got != testCase.expect {
 			t.Errorf("Incorrect number of trails found. got: %v, expect %v", got, testCase.expect)
 		}

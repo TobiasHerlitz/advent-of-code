1d420a00dfc2f9f8cb91670f841b97532582e3aa
Tobias Herlitz <tobiasherlitz@gmail.com>
2024-12-19 11:59:50 +0100

    day 11 part 1 done

--- 2024/11/main.go
+++ 2024/11/main.go
@@ -1,27 +1,72 @@
-/*
- * 1. If the number is 0 --> 1
- * 2. If the stone number has even digits --> split in two ([1000] -> [10, 0])
- * 3. Else --> multiply original by 2024
- */
-
 package main
 
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 )
 
 type Stones []int
-var input = Stones{0, 5601550, 3914, 852, 50706, 68, 6, 645371}
 
-func (s Stones) blink() {
-	return
+func hasEvenDigits(num int) bool {
+	if num == 0 {
+		return true
+	}
+
+	return len(strconv.Itoa(num))%2 == 0
+}
+
+// blink modifies the stones based on the following rules
+// Replaces 0 with 1
+// Splits numbers with even number of digits into two stones
+// Else multiplies original by 2024
+func (s *Stones) blink() error {
+	for i := 0; i < len(*s); i++ {
+		switch {
+		case (*s)[i] == 0:
+			(*s)[i] = 1
+		case hasEvenDigits((*s)[i]):
+			stoneStr := strconv.Itoa((*s)[i])
+			leftStone, err := strconv.Atoi(stoneStr[:len(stoneStr)/2])
+			if err != nil {
+				return err
+			}
+
+			rightStone, err := strconv.Atoi(stoneStr[len(stoneStr)/2:])
+			if err != nil {
+				return err
+			}
+
+			(*s)[i] = leftStone
+			*s = slices.Insert(*s, i+1, rightStone)
+			i++ // Skip newly inserted stone
+		default:
+			(*s)[i] *= 2024
+		}
+	}
+
+	return nil
+}
+
+func (s *Stones) blinkTimes(times int) error {
+	for i := 0; i < times; i++ {
+		err := s.blink()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func main() {
-	for i := 0; i < 6; i++ {
-		input.blink()
+	stones := Stones{0, 5601550, 3914, 852, 50706, 68, 6, 645371}
+	err := stones.blinkTimes(25)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error when blinking. Original:", err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("Part 1 - Total number of stones after blinking 25 times is: %v\n", 22)
+	fmt.Printf("Part 1 - Total number of stones after blinking 25 times is: %v\n", len(stones))
 }
--- 2024/11/main_test.go
+++ 2024/11/main_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBlinkOnce(t *testing.T) {
+	stones := Stones{125, 17}
+	expect := Stones{253000, 1, 7}
+	stones.blink()
+
+	if !reflect.DeepEqual(stones, expect) {
+		t.Errorf("Stones are incorrect. got: %v, expect %v", stones, expect)
+	}
+}
+
+func TestBlink6Times(t *testing.T) {
+	stones := Stones{125, 17}
+	expect := Stones{2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2}
+	stones.blinkTimes(6)
+
+	if !reflect.DeepEqual(stones, expect) {
+		t.Errorf("Stones are incorrect. got: %v, expect %v", stones, expect)
+	}
+}
+
+func TestBlink25Times(t *testing.T) {
+	stones := Stones{125, 17}
+	expect := 55312
+	stones.blinkTimes(25)
+
+	got := len(stones)
+
+	if !reflect.DeepEqual(got, expect) {
+		t.Errorf("Number of stones are incorrect. got: %v, expect %v", got, expect)
+	}
+}

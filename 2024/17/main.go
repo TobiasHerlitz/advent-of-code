e0ca1367dbbc069ca07cb7aa2a0c8008c9c6b538
Tobias Herlitz <tobiasherlitz@gmail.com>
2024-12-19 10:41:42 +0100

    some thoughts on day 17 part 2

--- 2024/17/main.go
+++ 2024/17/main.go
@@ -1,5 +1,3 @@
-// If a program outputs multiple values, they are separated by commas
-
 package main
 
 import (
@@ -28,7 +26,23 @@ var registers = Registers{
 	C: 0,
 }
 
+// var program = []int{2, 4, 1, 3, 7, 5, 0, 3, 1, 4, 4, 7, 5, 5, 3, 0}
 var program = []int{2, 4, 1, 3, 7, 5, 0, 3, 1, 4, 4, 7, 5, 5, 3, 0}
+
+// 1. Store registerA value in registerB
+// 2. Bitwise XOR on RegisterB and 3, store in RegisterB
+// 3. Store registerB value in registerC
+// 4. Divide registerA by 8
+// 5. Store registerA value in registerB
+// 6. Bitwise XOR on registerB and registerC, store in registerB
+// 7. Output mod 8 of registerB
+// 8. Restart until registerA is 0
+
+// 2. Bitwise XOR on RegisterA and 3, store in RegisterB
+// 4. Divide registerA by 8
+// 6. Bitwise XOR on registerA and registerB, store in registerB
+// 7. Output mod 8 of registerB
+// 8. Restart until registerA is 0
 
 // Opcodes 0, 6, and 7.
 func (r *Registers) division(writeTarget RegisterID, operand int) {
@@ -124,11 +138,42 @@ func runProgram(program []int, registers *Registers) (string, error) {
 	return strings.Join(outputs, ","), nil
 }
 
+// Thoughts:
+// Out should execute 16 times
+// The program has to end with [3, x]. Right?
+// The program is going to "bounce" on the final [3, x] until register a is 0
+// A is only written to on [0, x]
+// The only time opcode 0 is used is [0, 3]. i.e. a divided by 8 (2^3)
+// 
+// Guess: Given that A is divided by 8, 16 times to produce a value that lets the program "escape"
+// So: A / 8^16 < 1
+// A < 8^16
+// 
+// And given that it needs to "bounce" 16 times.
+// So: A / 8^15 > 1
+// A > 8^15
+// 
+// 35 184 372 088 832 < a < 281 474 976 710 656
+// Yeeee, thats not reasonable
+func searchForLowestA(program []int) (int, error) {
+
+	// return 0, fmt.Errorf("Failed finding lowest A")
+	return 0, nil
+}
+
 func main() {
 	output, err := runProgram(program, &registers)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed running program:", err)
 		os.Exit(1)
 	}
+
+	lowestA, err := searchForLowestA(program)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed searching for lowest A:", err)
+		os.Exit(1)
+	}
+
 	fmt.Printf("Part 1 - Output of program is: %v\n", output)
+	fmt.Printf("Part 2 - Lowest A that outputs an identical program is: %v\n", lowestA)
 }
